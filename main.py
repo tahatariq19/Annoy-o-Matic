7f2c40b735941e9b6b5f599536144fe0c25d0a4b
tahatariq19 <tahatariq19@protonmail.com>
2025-06-30 06:00:09 +0400

    Redid comments, described setup

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+#Import libraries
 import os
 import discord
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
+# Loads environment variables from .env file
 load_dotenv()
 
-# Get the token from the environment variables
+# Loads your bot api token from the .env file, make sure to set it up:
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-# Define your user IDs and their corresponding replies
-# Fetching environment variables directly into the dictionary
+# --- YOU SHOULD CONFIGURE THIS PART --- #
+# Here, you set the victims discord user_id, followed by the message you want to annoy them with.
+# The user_id is fetched from the .env file, same as the DISCORD_TOKEN key. Make sure to set it up.
+# Afterwards, you can adjust the following template, and add your annoyances.
 user_replies = {
-    int(os.getenv('USER_ID_1')): "Cutie", 
-    int(os.getenv('USER_ID_2')): "U Maniac",
+    int(os.getenv('USER_ID_1')): "Loser", 
+    int(os.getenv('USER_ID_2')): "U Cutie",
     int(os.getenv('USER_ID_3')): "U Imbecile",
-    int(os.getenv('USER_ID_4')): "King Man",
+    int(os.getenv('USER_ID_4')): "Woah, a noob appears!",
 }
 
 intents = discord.Intents.default()
@@ -32,15 +35,17 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    # Grabs sender's user ID and checks for a corresponding reply message.
     reply_message = user_replies.get(message.author.id)
 
-    if reply_message:  # Check if a reply message exists for the author
+    if reply_message:  # Check if a reply message exists for the user_id
         try:
-            await message.reply(reply_message)
-            print(f"Replied to {message.author.display_name} in #{message.channel.name}")
+            await message.reply({reply_message}) #Replies to the victim with annoyance :P
+            print(f"Replied to {message.author.display_name} in {message.channel.name}")
         except discord.Forbidden:
             print(f"Lacked permissions to reply in {message.channel.name}")
         except Exception as e:
             print(f"An error occurred: {e}")
+
 
 client.run(DISCORD_TOKEN)
