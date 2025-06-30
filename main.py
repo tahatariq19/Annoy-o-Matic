38d1d01fa8cc8e356d36650b4b13773840862373
tahatariq19 <tahatariq19@protonmail.com>
2025-06-30 09:23:59 +0500

    Added error handling for getenv variables.

--- main.py
+++ main.py
@@ -8,17 +8,27 @@ load_dotenv()
 
 # Loads your bot api token from the .env file, make sure to set it up:
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+if DISCORD_TOKEN is None:
+    raise ValueError("DISCORD_TOKEN is not set in the environment variables.")
 
 # --- YOU SHOULD CONFIGURE THIS PART --- #
 # Here, you set the victims discord user_id, followed by the message you want to annoy them with.
 # The user_id is fetched from the .env file, same as the DISCORD_TOKEN key. Make sure to set it up.
 # Afterwards, you can adjust the following template, and add your annoyances.
-user_replies = {
-    int(os.getenv('USER_ID_1')): "Loser", 
-    int(os.getenv('USER_ID_2')): "U Cutie",
-    int(os.getenv('USER_ID_3')): "U Imbecile",
-    int(os.getenv('USER_ID_4')): "Woah, a noob appears!",
-}
+user_replies = {}
+user_id_1 = os.getenv('USER_ID_1')
+user_id_2 = os.getenv('USER_ID_2')
+user_id_3 = os.getenv('USER_ID_3')
+user_id_4 = os.getenv('USER_ID_4')
+
+if user_id_1 is not None:
+    user_replies[int(user_id_1)] = "Loser"
+if user_id_2 is not None:
+    user_replies[int(user_id_2)] = "U Cutie"
+if user_id_3 is not None:
+    user_replies[int(user_id_3)] = "U Imbecile"
+if user_id_4 is not None:
+    user_replies[int(user_id_4)] = "Woah, a noob appears!"
 
 intents = discord.Intents.default()
 intents.message_content = True  # Required to read message content
@@ -47,5 +57,4 @@ async def on_message(message):
         except Exception as e:
             print(f"An error occurred: {e}")
 
-
-client.run(DISCORD_TOKEN)
+client.run(DISCORD_TOKEN)  # type: ignore[arg-type]
